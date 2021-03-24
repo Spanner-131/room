19e63bf3313bc75426a3835f6fc4d3e4b3185855
Spanner-131 <13512105153@163.com>
2021-03-24 16:56:10 +0800

    联表sql，未测试

--- vedioWebsite/src/main/java/website/mapper/VedioMapper.java
+++ vedioWebsite/src/main/java/website/mapper/VedioMapper.java
@@ -1,9 +1,16 @@
 package website.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 import website.pojo.Vedio;
+import website.vo.VedioVo;
+
+import java.util.List;
 
 @Repository
 public interface VedioMapper extends BaseMapper<Vedio> {
+
+    @Select("Select USER_NAME,HEADIMG,URL,TITLE,DESCRIPTION,COVER_URL from vedio v,user u where v.userCode = u.userCode")
+    List<VedioVo> vedioInfo();
 }
